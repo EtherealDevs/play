b206a43f78374064eb47073df1535316e19fe889
Rodrigo <gatopunk99@gmail.com>
2023-08-23 21:51:13 -0300

    BACKEND views - routes - controllers

--- reactfront/tailwind.config.js
+++ reactfront/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = withMT ({
     "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'  
   ],
   theme: {
     screens: {
